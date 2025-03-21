944b5094fac0d95d9b668598bc82e1ba86403257
Andrei Savolainen <andrei.savolainen@gmail.com>
2025-03-21 09:51:03 +0200

    added cypress tests

--- frontend/cypress/e2e/home.cy.ts
+++ frontend/cypress/e2e/home.cy.ts
@@ -12,4 +12,11 @@ describe("Countries Application", () => {
   it("Shows a list of countries", () => {
     cy.findByRole("link", { name: "Countries" }).click();
   });
+  it("More than 200 countries are displayed", () => {
+    cy.findByRole("link", { name: "Countries" }).click();
+    cy.get(".MuiCard-root").should("have.length.greaterThan", 200);
+  });
+  //   it("Can find Grenada", () => {
+  //     cy.findByValue("Grenada").should("exist");
+  //   });
 });
